3113167489e686ab03b7f3a981eef6d278c68a84
Raja Rao DV <rajaraodv100@gmail.com>
2018-10-10 14:50:49 -0700

    first commit

---
+++ stories/index.js
@@ -0,0 +1,30 @@
+import { storiesOf } from '@storybook/vue';
+import { linkTo } from '@storybook/addon-links';
+
+import MyButton from './MyButton';
+import Welcome from './Welcome';
+import App from '../src/components/App.vue';
+
+storiesOf('Welcome', module).add('to Storybook', () => ({
+  components: { Welcome },
+  template: '<welcome :showApp="action" />',
+  methods: { action: linkTo('Button') },
+}));
+
+storiesOf('Button', module)
+  .add('with text', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">Hello Button</my-button>',
+    methods: { action: linkTo('clicked') },
+  }))
+  .add('with some emoji', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: linkTo('clicked') },
+  }));
+
+  storiesOf('App', module).add('Default', () => ({
+    components: { App },
+    template: '<App  />',
+    methods: { action: linkTo('clicked') },
+  }));
